b7bffe634323211073df747284ec03e3acf149c1
CcYyQq <34188141+CcYyQq@users.noreply.github.com>
2018-03-13 22:13:53 +0800

    Update 00_get_cellbarcode.py

--- 00_get_cellbarcode.py
+++ 00_get_cellbarcode.py
@@ -3,9 +3,6 @@ import re, sys, time, json
 import subprocess
 import cPickle as pickle
 
-def HammingDistance(seq1, seq2):
-    return sum([1 for x in zip(seq1, seq2) if x[0]!=x[1]])
-
 def get_barcode(protocol, seq):
     if protocol == "10X":
         return seq[0:16]
@@ -25,11 +22,13 @@ def get_barcode(protocol, seq):
                     break
         return ""
 
-configFile = sys.argv[1]
-readFile = sys.argv[2]
-output = sys.argv[3]
-protocol = sys.argv[4]
-min_reads = int(sys.argv[5])
+
+readFile = sys.argv[1]
+output = sys.argv[2]
+protocol = sys.argv[3]
+min_reads = int(sys.argv[4])
+
+starttime = time.time()
 
 #Prepare The Output
 infos = {}
@@ -40,7 +39,6 @@ with open(configFile, 'r') as infile:
 
 #Read Files
 bc_counts = {}
-initTime = time.time()
 starttime = time.time()
 process_reading = subprocess.Popen(["zcat", readFile], stdout = subprocess.PIPE, bufsize=100*1000000)
 infile = process_reading.stdout
@@ -59,9 +57,9 @@ while True:
         bc_counts[bc] += 1
     else:
         bc_counts[bc] = 1
-    if index % 1000000 == 1:
-        print("Processed", index-1, time.time() - starttime)
-        starttime = time.time()
+    #if index % 1000000 == 1:
+       # print("Processed", index-1, time.time() - starttime)
+       # starttime = time.time()
 
 bc_counts = {k:v for k,v in bc_counts.items() if v>=min_reads}
 
@@ -71,3 +69,5 @@ infos['filter_low_depth'] = sum(bc_counts.values())
 
 with open(output, 'wb') as outfile:
     pickle.dump(infos, outfile)
+endtime = time.time()
+print(endtime - starttime)
